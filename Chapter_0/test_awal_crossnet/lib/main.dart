3dd293c1c91cd9f4b1dde41d465d2a7d3aaa95ee
Yestoya Minggus <yestoya.minggus@gmail.com>
2025-01-07 18:14:32 +0700

    tugas

--- Chapter_0/test_awal_crossnet/lib/main.dart
+++ Chapter_0/test_awal_crossnet/lib/main.dart
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
+    // final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
---
+++ Chapter_0/tugas_layouting/android/app/src/main/kotlin/com/example/tugas_layouting/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas_layouting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
